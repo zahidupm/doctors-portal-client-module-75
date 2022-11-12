a630c17ff0feb915154323eea3b7fd7a01e7b435
zahidupm <zahidhasanlimon111@gmail.com>
2022-11-12 12:27:37 +0600

    default navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { router } from './routes/routes/routes';
 
 function App() {
   return (
-    <div className="">
+    <div className="max-w-[1440px] mx-auto">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
