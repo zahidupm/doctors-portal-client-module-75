8e12ccdf57ccc309bd6c55b3646a35c4dd33cdd9
zahidupm <zahidhasanlimon111@gmail.com>
2022-11-12 15:08:10 +0600

    banner design

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import chair from '../../../assets/images/chair.png';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="hero">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="rounded-lg w-1/2 shadow-2xl" alt='' />
+                <div>
+                <h1 className="text-5xl font-bold">Box Office News!</h1>
+                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <button className="btn btn-primary bg-gradient-to-r from-primary text-white to-secondary">Get Started</button>
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary: '#19D3AE',
+          accent: "#3A4256",
+          neutral: "#291334",  
+          "base-100": "#FAF7F5",
+        }
+      }
+    ]
+  },
   theme: {
     extend: {},
   },
