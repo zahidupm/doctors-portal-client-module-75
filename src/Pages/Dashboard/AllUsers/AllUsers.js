bc065b00514da492f539b3e16c3defece7ae5a32
zahidupm <zahidhasanlimon111@gmail.com>
2022-11-18 14:39:35 +0600

    create make admin API limit the     feature based on admin level

--- src/Pages/Dashboard/AllUsers/AllUsers.js
+++ src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllUsers = () => {
 
-    const {data: users = []} = useQuery({
+    const {data: users = [], refetch} = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await fetch(`http://localhost:5000/users`);
@@ -11,6 +12,22 @@ const AllUsers = () => {
             return data;
         }
     });
+
+    const handleMakeAdmin = (id) => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: 'PUT', 
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.modifiedCount > 0) {
+                toast.success('Make admin successful');
+                refetch();
+            }
+        })
+    }
 
     return (
         <div>
@@ -32,7 +49,7 @@ const AllUsers = () => {
                             <th>{i+1}</th>
                             <td>{user.name}</td>
                             <td>{user.email}</td>
-                            <td><button className='btn btn-xs btn-primary text-white'>Make Admin</button></td>
+                            <td>{ user?.role !== 'admin' && <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-xs btn-primary text-white'>Make Admin</button>}</td>
                             <td><button className='btn btn-xs btn-danger'>Delete</button></td>
                         </tr>)
                     }
