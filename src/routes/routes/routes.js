b6bec1db34fc24c8024c9e47e911a0a60f9509c5
zahidupm <zahidhasanlimon111@gmail.com>
2022-11-18 11:16:53 +0600

    load allusers in table

--- src/routes/routes/routes.js
+++ src/routes/routes/routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../../layout/DashboardLayout";
 import Main from "../../layout/Main";
 import Appointment from "../../Pages/Appointment/Appointment/Appointment";
+import AllUsers from "../../Pages/Dashboard/AllUsers/AllUsers";
 import MyAppointment from "../../Pages/Dashboard/MyAppointment/MyAppointment";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
@@ -38,6 +39,10 @@ export const router = createBrowserRouter([
             {
                 path: '/dashboard',
                 element: <MyAppointment></MyAppointment>
+            },
+            {
+                path: '/dashboard/all_users',
+                element: <AllUsers></AllUsers>
             }
         ]
     }
